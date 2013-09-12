3a23c2870ac4f584d886aee35b38a5b74afd2b62
si-integration <xavi.hidalgo.fernandez@gmail.com>
2013-09-12 13:49:06 +0200

    clean log4j and dictionary capabilities

--- src/main/java/com/socialintellegentia/entryPoints/spider.java
+++ src/main/java/com/socialintellegentia/entryPoints/spider.java
@@ -5,7 +5,9 @@ import com.androidxtrem.spider.si.SpiderSiRSS;
 import com.socialintellegentia.commonhelpers.hibernate.SpiderPersistence;
 import com.socialintellegentia.processes.ProcessRSS;
 import com.socialintellegentia.util.JsonHelper;
+import org.json.JSONException;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -26,40 +28,63 @@ public class spider {
             return;
         }
         String sourceFile = args[0];
+        SpiderPersistence spiderPersistence = new SpiderPersistence();
 
-        try
-        {
-            boolean fileExists = FileHelper.fileExists(sourceFile);
-            String rssSourcesJson = (fileExists) ? FileHelper.fileToString(sourceFile) : "[{\"source\":\""+sourceFile+"\"}]";
+        List<String> rssSources = null;
+        try {
+            rssSources = getRssSources(sourceFile);
+        } catch (IOException e) {
+            System.out.println("Error openning file: ["+rssSources+"]\n["+e.getMessage()+"]");
+        } catch (JSONException e) {
+            System.out.println("Error in JSON parsing: [" + e.getMessage() + "]");
+        }
 
-            SpiderPersistence spiderPersistence = new SpiderPersistence();
+        SpiderSiRSS runner = null;
 
-            List<String> rssSources = JsonHelper.getRssSourcesFromJson(rssSourcesJson);
+        try {
+            runner = new SpiderSiRSS();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
 
 
-            for (String rssSource : rssSources) {
-                if (spiderPersistence.isUrlInBlackList(rssSource)) {
-                    System.out.println("\n" +
-                            "\n____________________________________________________________________" +
-                            "\n B L A C K L I S T E D  ==>  " + rssSource +
-                            "\n____________________________________________________________________" +
-                            "\n");
-                    continue;
-                }
-
-                ProcessRSS processRSS = new ProcessRSS();
-                SpiderSiRSS runner = new SpiderSiRSS();
-                runner.getNewsFromRSSserver(rssSource);
-                runner.stop();
-                String workingFolder = runner.getWorkingFolder();
-                processRSS.processRSSfromWorkingDirectory(workingFolder);
-
+        for (String rssSource : rssSources) {
+            if (spiderPersistence.isUrlInBlackList(rssSource)) {
+                System.out.println("\n" +
+                        "\n____________________________________________________________________" +
+                        "\n B L A C K L I S T E D  ==>  " + rssSource +
+                        "\n____________________________________________________________________" +
+                        "\n");
+                continue;
             }
+
+            try {
+                runner.getNewsFromRSSserver(rssSource);
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } catch (InterruptedException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+
+//            runner.stop();
+
+//            try {
+//                String workingFolder = runner.getWorkingFolder();
+//                ProcessRSS processRSS = new ProcessRSS();
+//                processRSS.processRSSfromWorkingDirectory(workingFolder);
+//            } catch (Exception e) {
+//                System.out.println("Error when running spider: ["+e.getMessage()+"]");
+//            }
         }
-        catch (Exception e){
-            System.out.println("Error when running spider: ["+e.getMessage()+"]");
-            e.printStackTrace();
-        }
+    }
+
+
+
+    private static List<String> getRssSources(String sourceFile) throws IOException, JSONException {
+        boolean fileExists = FileHelper.fileExists(sourceFile);
+        String rssSourcesJson = (fileExists) ? FileHelper.fileToString(sourceFile) : "[{\"source\":\""+sourceFile+"\"}]";
+        return JsonHelper.getRssSourcesFromJson(rssSourcesJson);
     }
 
     private static void printUsage() {
